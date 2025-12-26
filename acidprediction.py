9cb4a69af18563eccb2fda8f37a3a9b04478ca5a
kushagraanand-prog <66543759+kushagraanand-prog@users.noreply.github.com>
2025-12-26 13:34:24 +0530

    Update acidprediction.py

--- acidprediction.py
+++ acidprediction.py
@@ -30,18 +30,24 @@ Predict **acid concentration (g/L)** using:
 # -----------------------------
 # User Inputs
 # -----------------------------
+
+min_temp = float(df["Temperature_C"].min())
+
 temperature = st.number_input(
     "Enter Temperature (°C):",
-    min_value=0.0,
-    value=15.0,
-    step=1.0,
+    min_value=min_temp,
+    value=min_temp,
+    step=0.1,
     format="%.1f"
 )
 
+
+min_cond = float(df["Conductivity_mS_cm"].min())
+
 conductivity_mS = st.number_input(
     "Enter Conductivity (mS/cm):",
-    min_value=0.0,
-    value=82.0,
+    min_value=min_cond,
+    value=min_value,
     step=1.0,
     format="%.0f"
 )
